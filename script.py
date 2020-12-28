3ad4d8840382a0fa1bc4ef0b55b91ae8b367371f
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2020-12-27 23:13:07 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,10 @@
+from tkinter import Message
+import pyautogui as p
+
+msg = input("¿Qué Mensaje desea enviar)\n")
+
+n = int(input("¿Cuántas copias del mensaje quiere enviar?\n"))
+
+for x in range(x):
+    p.write("Holaaaa")
+    p.press("enter")
