9c608fef2fa78e0e243ff5e20a511e1e74aedae0
Sebastián Pachón <55819947+jupachonc@users.noreply.github.com>
2021-03-04 23:29:47 -0500

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ msg = input("¿Qué Mensaje desea enviar?\n")
 
 n = int(input("¿Cuántas copias del mensaje quiere enviar?\n"))
 
-for x in range(x):
+for x in range(n):
     p.write(msg)
     p.press("enter")
